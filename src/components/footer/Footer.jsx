d1e3fddae5d1461dc74e08f27a340e80326b66dd
iksanh <iksanhariji@gmail.com>
2023-10-18 16:05:37 +0800

    fix logo Problem

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./footer.style.scss";
-import Logo from "../../img/Logo.png";
+import Logo from "../../img/logo.png";
 
 const Footer = (props) => {
   return (
